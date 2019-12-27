987288aa846dcd3ce518f7cdcea47af7628c7ebd
yanjy <1019975412@qq.com>
2019-12-27 22:02:31 +0800

    数据绑定+button简单实现

--- pages/hello.js
+++ pages/hello.js
@@ -1,4 +1,28 @@
 Page({
+  data:{
+    name: 'world!',
+    classmatename: 'XiaoMing',
+    students:[
+      {name:'code', age:18},
+      {name:'jam', age:19},
+      {name:'xiaoli', age:20}
+      ],
+    number:0
+    },
+    handleClickadd(){
+      //this.data.number += 1
+      this.setData(
+        {
+          number: this.data.number += 1
+        }
+      )
+      console.log("点击了按钮", this.data.number)
+    },
+  handleClickdel(){
+    this.setData({
+      number: this.data.number -= 1
+    })
+    //console.log(this.data.number--)
+  }
 
-  
 })
