905e8157a27cae40011a8256591f5127730d0d93
yanjy <1019975412@qq.com>
2020-01-02 18:35:55 +0800

    增加云函数获取数据，解除一次只能获取20条数据的限制

---
+++ cloud/getArticle/index.js
@@ -0,0 +1,13 @@
+// 云函数入口文件
+const cloud = require('wx-server-sdk')
+
+cloud.init({
+  env: "hello-rqbjk"
+})
+
+// 云函数入口函数
+exports.main = async (event, context) => {
+  //const wxContext = cloud.getWXContext()
+  
+  return cloud.database().collection('list').get();
+}
--- pages/hello/hello.js
+++ pages/hello/hello.js
@@ -4,6 +4,23 @@
   },
   onShow:function(options){
     var that = this
+    wx.cloud.callFunction({
+      name: "getArticle",
+
+      success(res) {
+        console.log("通过云函数获取数据成功", res)
+        that.setData({
+          artical: res.result.data
+        })
+      },
+      fail(res) {
+        console.log("通过云函数保存失败", res)
+      }
+
+    })
+  }
+  /*onShow:function(options){
+    var that = this
     wx.cloud.database().collection("list").get({
       success: function (res) {
         // res.data 包含该记录的数据
@@ -14,7 +31,7 @@
         //artical = res.data[0].content
       }
     })
-  },
+  }*/,
   handleWrite(){
     console.log("写笔记")
     wx.navigateTo({
